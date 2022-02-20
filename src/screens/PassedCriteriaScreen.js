9fd772f25500bc5f9bbcaf9279c276984d767f82
danica-du <ddu@hmc.edu>
2022-02-19 16:18:28 -0800

    SmallLogoHeader component, PassedCriteria WIP

---
+++ src/screens/PassedCriteriaScreen.js
@@ -0,0 +1,13 @@
+import './styles/PassedCriteria.css';
+import SmallLogoHeader from '../components/SmallLogoHeader';
+
+function PassedCriteriaScreen() {
+
+    return (
+        <div className="pc-container">
+            <SmallLogoHeader />
+        </div>
+    );
+}
+
+export default PassedCriteriaScreen;
